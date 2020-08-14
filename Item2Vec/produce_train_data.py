c2a2984a7a2ee944b30994b6619902f4b569ccf9
qiusuor <750435412@qq.com>
2020-08-14 16:37:39 +0800

    item2vec

---
+++ Item2Vec/produce_train_data.py
@@ -0,0 +1,30 @@
+import sys
+import os
+def produce_train_data(input_file,out_file):
+    """
+    从用户的评分文件，获取训练文件
+    :param input_file:  user behavior file
+    :param out_file:
+    :return:
+    """
+    if not os.path.exists(input_file):
+        return
+    record = {}
+    score_thr=4
+    with open(input_file ,"r",encoding="utf-8") as file:
+        for line in file.readlines()[1:]:
+            item = line.strip().split(",")
+            if len(item) <4:
+                continue
+            user_id,item_id,rating = item[0],item[1],float(item[2])
+            if rating <float(score_thr):
+                continue
+            if user_id not in record:
+                record[user_id] = []
+            record[user_id].append(item_id)
+    with open(out_file,"w",encoding="utf-8") as w_file:
+        for userid in record:
+            w_file.write(" ".join(record[userid]) +"\n")
+
+if __name__ == "__main__":
+    produce_train_data("data/ratings.txt","data/train_data.txt")
